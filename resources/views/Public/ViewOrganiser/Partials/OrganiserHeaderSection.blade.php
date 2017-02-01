4b574614eecac39e7988e976fcb97a685555700d
Matt Sims <matt@deucecreative.co.uk>
2017-02-01 19:09:09 +0000

    Only show organiser name if no custom logo set.

--- resources/views/Public/ViewOrganiser/Partials/OrganiserHeaderSection.blade.php
+++ resources/views/Public/ViewOrganiser/Partials/OrganiserHeaderSection.blade.php
@@ -6,7 +6,9 @@
                     <img src="{{$organiser->full_logo_path}}" />
                 </div>
             </div>
+            @if($organiser->full_logo_path === config('attendize.fallback_organiser_logo_url'))
             <h1>{{$organiser->name}}</h1>
+            @endif
             @if($organiser->about)
             <div class="description pa10">
                 {!! $organiser->about !!}
