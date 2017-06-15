a9c205b757a5091186f08ab315e35bdb8c328a52
Matt Sims <matt@deucecreative.co.uk>
2017-06-15 21:52:13 +0100

    Use Google Maps Javascript API instead of iFrame embed, which causes redirect issues in mobile Facebook browser.

--- resources/views/Public/ViewEvent/Partials/EventMapSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventMapSection.blade.php
@@ -2,7 +2,40 @@
     <div class="row">
         <div class="col-md-12">
             <div class="google-maps content">
-                <iframe frameborder="0" style="border:0;" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q={{$event->map_address}}&amp;aq=0&amp;oq={{$event->map_address}}&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq={{$event->map_address}}&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+                <div id="map"></div>
+                <script>
+                    function initMap() {
+                        geocoder = new google.maps.Geocoder();
+
+                        var london = {lat: 54.093409, lng: -2.89479};
+                        var map = new google.maps.Map(document.getElementById('map'), {
+                            zoom: 5,
+                            center: london
+                        });
+                        var geocoder = new google.maps.Geocoder();
+
+                        geocodeAddress(geocoder, map);
+                    }
+
+                    function geocodeAddress(geocoder, resultsMap) {
+                        var address = decodeURIComponent('{{$event->map_address}}') + ',UK';
+
+                        address = address.replace(/^[,\s]+|[,\s]+$/g, '').replace(/,[,\s]*,/g, ',');
+
+                        geocoder.geocode({'address': address}, function(results, status) {
+                            if (status === 'OK') {
+                                resultsMap.setCenter(results[0].geometry.location);
+                                var marker = new google.maps.Marker({
+                                    map: resultsMap,
+                                    position: results[0].geometry.location
+                                });
+
+                                resultsMap.setZoom(15)
+                            }
+                        });
+                    }
+                </script>
+                <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJ8-z8GaLziXfz_MCnwL0_HLdw2QgnGes&callback=initMap"></script>
             </div>
         </div>
     </div>
